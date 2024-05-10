52eaeb0f46c763ef37f54ba916c775dacd33b540
kyle-ssg <kyle@solidstategroup.com>
2024-05-10 20:09:26 +0100

    Add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
 'use client'
 export default function Home() {
-  return <img style={{width:"100%"}} src='/api/image'/>
+  return <div
+  style={{
+    fontFamily:'-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  }}
+  >
+
+    <div style={{maxWidth:850, borderRadius:8, padding:25, marginBottom:50, background:'#22272E'}}>
+      <h3 style={{
+        color:'rgb(197, 209, 222)',
+      }}>Dark Mode</h3>
+      <img style={{width:"100%"}} src='/api/image'/>
+    </div>
+    <div style={{maxWidth:850, borderRadius:8, padding:25, marginBottom:50,}}>
+      <h3>Light Mode</h3>
+      <img style={{width:"100%"}} src='/api/image'/>
+    </div>
+
+  </div>
 }
