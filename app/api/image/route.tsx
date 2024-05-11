2d6863f5bd1600b1e2c9df363ebb0b8118ae9d5f
kyle-ssg <kyle@solidstategroup.com>
2024-05-11 09:25:27 +0100

    Style

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -22,16 +22,16 @@ const colours = {
         },
         red: {
             500: "rgb(196,64,104)",
-            400: "#eb6d5a"
+            400: "#eb5f5a"
         },
         green: {
             500: "#57AB5A",
-            400: "#65ab57"
+            400: "#6dc370"
         }
     }
 }
 
-export async function GET(req:  NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
 
     console.log(req.headers.get("cookie"))
     // Make sure the font exists in the specified path:
@@ -74,87 +74,116 @@ export async function GET(req:  NextRequest, res: NextResponse) {
         (
             <div
                 style={{
-                    display:"flex",
-                    flexDirection:"column",
-                    padding:10,
-                    backgroundColor:'#22272E',
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: 10,
+                    backgroundColor: '#22272E',
                     borderRadius: 8,
-                    fontFamily:'-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+                    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
                 }}
             >
                 <div style={{
                     display: "flex",
-                    flexWrap:'wrap',
+                    flexWrap: 'wrap',
                     flexDirection: "row",
-                }}>
+                }}
+                >
                     {items.map((v, i) => {
-                    const colourKey = Object.keys(colours.main)[i]
-                    const color = colours.main[colourKey as keyof typeof colours.main][500]
-                    return (
-                    <div key={i} style={{
-                        width: "100%",
-                        fontSize:16,
-                        fontWeight: 500,
-                        flex: "0 0 auto",
-                        padding: 4,
-                        display:"flex",
-                    }}
-                    >
-                        <div style={{
-                            width:"100%",
-                            padding:12,
-                            borderRadius,
-                            display:"flex",
-                            border: `1px solid ${colours.border}`,
-                            justifyContent:"space-between",
-                            alignItems:'center',
-                        }}>
-                            <div style={
-                                {
-                                    display:"flex",
-                                    color:colours.title
-                                }
-                            }>
-                                {`${v.environment_name}`}
-                            </div>
-                            <div style={{
-                                display:"flex",
-                                alignItems:'center',
-                                gap: 8
-                            }}>
-                                {v.feature_state_value && (
-                                    <span style={{
-                                        borderRadius,
-                                        backgroundColor: 'rgba(29, 18, 62, 0.5)',
-                                        border:'1px solid rgba(149, 108, 255, 0.32)',
-                                        color:'rgb(144, 106, 246)',
-                                        textWrap:'nowrap',
-                                        display:"flex",
-                                        padding:"5px 12px 5px 12px"
-                                    }}>
-                                        {v.feature_state_value}
-                                    </span>
-                                )}
+                        const colourKey = Object.keys(colours.main)[i]
+                        const color = colours.main[colourKey as keyof typeof colours.main][500]
+                        const color2 = colours.main[colourKey as keyof typeof colours.main][400]
+                        return (
+                            <div key={i} style={{
+                                width: "100%",
+                                fontSize: 16,
+                                fontWeight: 500,
+                                flex: "0 0 auto",
+                                padding: 4,
+                                display: "flex",
+                            }}
+                            >
                                 <div style={{
-                                    width:41,
-                                    height:24,
-                                    borderRadius:100,
-                                    backgroundColor:'rgb(104, 55, 252)',
-                                    color: 'rgb(144, 106, 246)',
-                                    padding:3,
-                                    display:'flex',
-                                    justifyContent:'flex-end',
-                                    alignItems:'center',
-                                }}>
-                                    <div style={{width:18,height:18,borderRadius:9,backgroundColor:'white'}}/>
+                                    width: "100%",
+                                    padding: 12,
+                                    borderRadius,
+                                    display: "flex",
+                                    border: `1px solid ${colours.border}`,
+                                    justifyContent: "space-between",
+                                    alignItems: 'center',
+                                }}
+                                >
+                                    <div style={
+                                        {
+                                            display: "flex",
+                                            alignItems: 'center',
+                                            gap: 8,
+                                            color: colours.title
+                                        }
+                                    }
+                                    >
+                                        <svg height={18} xmlns="http://www.w3.org/2000/svg" className="ionicon"
+                                             viewBox="0 0 512 512"
+                                        >
+                                            <defs>
+                                                <radialGradient id="grad1" cx="50%" cy="55%" r="50%">
+                                                    <stop offset="90%" style={{stopColor:color}} />
+                                                    <stop offset="100%" style={{stopColor:color2}} />
+                                                </radialGradient>
+                                            </defs>
+                                            <path
+                                                fill="url(#grad1)"
+                                                d="M416 160a64 64 0 10-96.27 55.24c-2.29 29.08-20.08 37-75 48.42-17.76 3.68-35.93 7.45-52.71 13.93v-126.2a64 64 0 10-64 0v209.22a64 64 0 1064.42.24c2.39-18 16-24.33 65.26-34.52 27.43-5.67 55.78-11.54 79.78-26.95 29-18.58 44.53-46.78 46.36-83.89A64 64 0 00416 160zM160 64a32 32 0 11-32 32 32 32 0 0132-32zm0 384a32 32 0 1132-32 32 32 0 01-32 32zm192-256a32 32 0 1132-32 32 32 0 01-32 32z"
+                                            ></path>
+                                        </svg>
+                                        {`${v.environment_name}`}
+                                    </div>
+                                    <div style={{
+                                        display: "flex",
+                                        alignItems: 'center',
+                                        gap: 8
+                                    }}
+                                    >
+                                        {v.feature_state_value && (
+                                            <span style={{
+                                                borderRadius,
+                                                backgroundColor: 'rgba(29, 18, 62, 0.5)',
+                                                border: '1px solid rgba(149, 108, 255, 0.32)',
+                                                color: 'rgb(144, 106, 246)',
+                                                textWrap: 'nowrap',
+                                                display: "flex",
+                                                padding: "5px 12px 5px 12px"
+                                            }}
+                                            >
+                                                {v.feature_state_value}
+                                            </span>
+                                        )}
+                                        <div style={{
+                                            width: 41,
+                                            height: 24,
+                                            borderRadius: 100,
+                                            backgroundColor: 'rgb(104, 55, 252)',
+                                            color: 'rgb(144, 106, 246)',
+                                            padding: 3,
+                                            display: 'flex',
+                                            justifyContent: 'flex-end',
+                                            alignItems: 'center',
+                                        }}
+                                        >
+                                            <div style={{
+                                                width: 18,
+                                                height: 18,
+                                                borderRadius: 9,
+                                                backgroundColor: 'white'
+                                            }}
+                                            />
+                                        </div>
+                                    </div>
+
                                 </div>
+
                             </div>
-
-                        </div>
-
-                    </div>
-                )
-                })}
+                        )
+                    })}
                 </div>
             </div>
         ),
@@ -182,6 +211,6 @@ export async function GET(req:  NextRequest, res: NextResponse) {
 
     headers.set("Content-Type", "image/svg+xml");
 
-    return new NextResponse(svg, { status: 200, statusText: "OK", headers });
+    return new NextResponse(svg, {status: 200, statusText: "OK", headers});
 
 }
