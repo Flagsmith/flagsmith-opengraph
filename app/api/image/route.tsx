b7b09a24244da3a15f55d68381c37c2d9939a689
kyle-ssg <kyle@solidstategroup.com>
2024-05-14 17:04:33 +0100

    Add cache

--- app/api/image/route.tsx
+++ app/api/image/route.tsx
@@ -6,15 +6,11 @@ export const runtime = 'edge';
 
 
 export async function GET(req: NextRequest, res: NextResponse) {
-
-
     const items = jose.decodeJwt(req.url.split("?t=")[1]).payload
     const svg = await generateSVG(items as any)
-
     const headers = new Headers();
-
     headers.set("Content-Type", "image/svg+xml");
-
+    headers.set("Cache-Control", "public, max-age=31536000");
     return new NextResponse(svg, {status: 200, statusText: "OK", headers});
-
 }
+export const revalidate = 31536000
